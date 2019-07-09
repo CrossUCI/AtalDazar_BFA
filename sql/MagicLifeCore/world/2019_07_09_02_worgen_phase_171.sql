d50c3ca21edb0cdf8256b021bd27752ab14f5c08
Mistixsnet <mistix20@gmail.com>
2019-07-09 13:02:17 +0300

    Fixed An area of ​​NPC Phases in Worgen Zone

---
+++ sql/MagicLifeCore/world/2019_07_09_02_worgen_phase_171.sql
@@ -0,0 +1,24 @@
+--Worgen phase 171
+UPDATE creature SET PhaseId = '171' WHERE id = 35077;
+UPDATE creature SET PhaseId = '171' WHERE id = 35123;
+UPDATE creature SET PhaseId = '171' WHERE id = 35081;
+UPDATE creature SET PhaseId = '171' WHERE id = 35124;
+UPDATE creature SET PhaseId = '171' WHERE id = 39095;
+UPDATE creature SET PhaseId = '171' WHERE id = 35232;
+UPDATE creature SET PhaseId = '171' WHERE id = 35118;
+UPDATE creature SET PhaseId = '171' WHERE id = 35167;
+UPDATE creature SET PhaseId = '171' WHERE id = 35170;
+UPDATE creature SET PhaseId = '171' WHERE id = 35456;
+UPDATE creature SET PhaseId = '171' WHERE id = 35188;
+UPDATE creature SET PhaseId = '171' WHERE id = 50371;
+
+DELETE FROM `creature` WHERE `guid`=0;
+DELETE FROM `creature` WHERE `guid`=210115267;
+DELETE FROM `creature` WHERE `guid`=20556551;
+DELETE FROM `creature` WHERE `guid`=20556543;
+DELETE FROM `creature` WHERE `guid`=20556486;
+DELETE FROM `creature` WHERE `guid`=20556550;
+DELETE FROM `creature` WHERE `guid`=20556475;
+DELETE FROM `creature` WHERE `guid`=20556472;
+DELETE FROM `creature` WHERE `guid`=20556473;
+DELETE FROM `creature` WHERE `guid`=20556533;
