547ec17b6f6a97fb50ea7225041b133e79c306f3
Mistixsnet <mistix20@gmail.com>
2019-07-09 14:35:48 +0300

    Core/Game: Fix Typo

--- src/server/game/Server/Packets/MiscPackets.h
+++ src/server/game/Server/Packets/MiscPackets.h
@@ -991,10 +991,10 @@ namespace WorldPackets
         public:
             StartTimer() : ServerPacket(SMSG_START_TIMER, 12) { }
 
-            enum TimeType: uint8
+            enum TimeType : uint8
             {
                 TIMER_TYPE_BATTLEGROUND = 0,
-                TIMER_TYPE_CHALLENGE    = 1,
+                TIMER_TYPE_CHALLENGE = 1,
             };
 
             WorldPacket const* Write() override;
