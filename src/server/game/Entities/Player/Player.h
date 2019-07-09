3f0fc387c0831de3d69a7155d53834c4298a6f9a
Mistixsnet <mistix20@gmail.com>
2019-07-09 15:19:54 +0300

    fix spell-script console database log failure. Part- 1

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1896,7 +1896,8 @@ class TC_GAME_API Player : public Unit, public GridObject<Player>
         void UpdateRating(CombatRating cr);
         void UpdateAllRatings();
         void UpdateMastery();
-        void UpdateVersatilityDamageDone();
+        float GetMasteryPct();
+		void UpdateVersatilityDamageDone();
         void UpdateHealingDonePercentMod();
         bool CanUseMastery() const;
         void UpdateAverageItemLevel();
