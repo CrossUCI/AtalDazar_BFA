efd14b8880a4f442021e904f8d6c460db0f914fa
David Gonzalez <david.gr79@gmail.com>
2019-07-14 17:26:25 -0600

    [SQL/UPDATES] 2019_07_10_00_fix_pandarens

--- sql/AtalDazarCore/updates/world/2019_07_10_00_fix_pandarens.sql
+++ sql/AtalDazarCore/updates/world/2019_07_10_00_fix_pandarens.sql
@@ -4,7 +4,7 @@ DELETE FROM `quest_objectives` WHERE `id`=263979;
 DELETE FROM `quest_objectives` WHERE `id`=263982;
 DELETE FROM `quest_objectives` WHERE `id`=263983;
 
-UPDATE `creature_template` SET `HealthModifier`=2  WHERE id = 54734;
+UPDATE `creature_template` SET `HealthModifier`=2  WHERE ENTRY = 54734;
 UPDATE `creature` SET `PhaseID`=993  WHERE `guid` = 21001920;
 UPDATE `creature` SET `PhaseID`=993  WHERE `id` = 55918;
 UPDATE `creature` SET `PhaseID`=993  WHERE `id` = 56662;
