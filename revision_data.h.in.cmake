5c2d5e19430e4d4a7b99a827e0029ce8d18dd307
Mistix <47088016+Mistixsnet@users.noreply.github.com>
2019-08-14 15:09:34 +0200

    Update revision_data.h.in.cmake

--- revision_data.h.in.cmake
+++ revision_data.h.in.cmake
@@ -11,7 +11,7 @@
  #define _MYSQL_EXECUTABLE          R"(@MYSQL_EXECUTABLE@)"
  #define _FULL_DATABASE             "ADB_world_820.00.sql"
  #define _HOTFIXES_DATABASE         "ADB_hotfixes_820.00.sql"
- #define VER_COMPANYNAME_STR        "Ashamane Developers"
+ #define VER_COMPANYNAME_STR        "AtalDazar Developers"
  #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2019 AshamaneCore"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
